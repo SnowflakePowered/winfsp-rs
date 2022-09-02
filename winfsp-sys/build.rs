a1a007c6a703762c7819fb4d74d0e5c7f688745e
chyyran <ronny@ronnychan.ca>
2022-09-02 13:11:10 -0400

    winfsp: allow link against system winfsp

--- winfsp-sys/build.rs
+++ winfsp-sys/build.rs
@@ -1,17 +1,58 @@
+#![feature(cfg_target_compact)]
+
+extern crate core;
+
+use registry::{Data, Hive, Security};
 use std::env;
 use std::path::PathBuf;
 
-fn main() {
-    // todo: find from HKLM\SOFTWARE\WOW6432Node\WinFsp;
+fn local() -> String {
     let project_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
 
     println!(
         "cargo:rustc-link-search={}",
         project_dir.join("winfsp/lib").to_string_lossy()
     );
+
+    "--include-directory=winfsp/inc".into()
+}
+
+fn system() -> String {
+    let winfsp_install = Hive::LocalMachine
+        .open("SOFTWARE\\WOW6432Node\\WinFsp", Security::Read)
+        .ok()
+        .and_then(|u| u.value("InstallDir").ok())
+        .expect("WinFsp installation directory not found.");
+    let directory = match winfsp_install {
+        Data::String(string) => string.to_string_lossy(),
+        _ => panic!("unexpected install directory"),
+    };
+
+    println!("cargo:rustc-link-search={}/lib", directory);
+
+    format!("--include-directory={}/inc", directory)
+}
+
+fn main() {
+    // todo: find from HKLM\SOFTWARE\WOW6432Node\WinFsp;
+
+    let link_include = if cfg!(feature = "system") {
+        system()
+    } else {
+        local()
+    };
+
     println!("cargo:rerun-if-changed=wrapper.h");
-    println!("cargo:rustc-link-lib=dylib=winfsp-x64");
-    println!("cargo:rustc-link-arg=/DELAYLOAD:winfsp-x64.dll");
+
+    if cfg!(target(os = "windows", arch = "x86_64", env = "msvc")) {
+        println!("cargo:rustc-link-lib=dylib=winfsp-x64");
+        println!("cargo:rustc-link-arg=/DELAYLOAD:winfsp-x64.dll");
+    } else if cfg!(target(os = "windows", arch = "i686", env = "msvc")) {
+        println!("cargo:rustc-link-lib=dylib=winfsp-x86");
+        println!("cargo:rustc-link-arg=/DELAYLOAD:winfsp-x86.dll");
+    } else {
+        panic!("unsupported triple")
+    }
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
@@ -24,8 +65,17 @@ fn main() {
         .allowlist_var("Fsp.*")
         .allowlist_var("CTL_CODE")
         .clang_arg("-DUNICODE")
-        .clang_arg("--include-directory=winfsp/inc")
-        .clang_arg("--target=x86_64-pc-windows-msvc")
+        .clang_arg(link_include);
+
+    let bindings = if cfg!(target(os = "windows", arch = "x86_64", env = "msvc")) {
+        bindings.clang_arg("--target=x86_64-pc-windows-msvc")
+    } else if cfg!(target(os = "windows", arch = "i686", env = "msvc")) {
+        bindings.clang_arg("--target=i686-pc-windows-msvc")
+    } else {
+        panic!("unsupported triple")
+    };
+
+    let bindings = bindings
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
--- winfsp/src/init.rs
+++ winfsp/src/init.rs
@@ -1,5 +1,7 @@
+use registry::{Data, Hive, Security};
+use windows::core::HSTRING;
 use windows::w;
-use windows::Win32::Foundation::ERROR_DELAY_LOAD_FAILED;
+use windows::Win32::Foundation::{ERROR_DELAY_LOAD_FAILED, ERROR_FILE_NOT_FOUND};
 use windows::Win32::System::LibraryLoader::LoadLibraryW;
 
 use crate::Result;
@@ -8,14 +10,66 @@ use crate::Result;
 #[derive(Copy, Clone)]
 pub struct FspInit;
 
-/// Initialize WinFSP.
-pub fn winfsp_init() -> Result<FspInit> {
+fn get_system_winfsp() -> Option<HSTRING> {
+    let winfsp_install = Hive::LocalMachine
+        .open("SOFTWARE\\WOW6432Node\\WinFsp", Security::Read)
+        .ok()
+        .and_then(|u| u.value("InstallDir").ok());
+    let mut directory = match winfsp_install {
+        Some(Data::String(string)) => string.to_os_string(),
+        _ => return None,
+    };
+
+    directory.push("\\bin\\");
+
+    if cfg!(target_arch = "x86_64") {
+        directory.push("winfsp-x64.dll");
+    } else if cfg!(target_arch = "i686") {
+        directory.push("winfsp-x86.dll");
+    } else {
+        panic!("unsupported arch")
+    }
+
+    Some(HSTRING::from(directory))
+}
+
+fn get_local_winfsp() -> HSTRING {
+    if cfg!(target_arch = "x86_64") {
+        w!("winfsp-x64.dll").clone()
+    } else if cfg!(target_arch = "i686") {
+        w!("winfsp-x86.dll").clone()
+    } else {
+        panic!("unsupported arch")
+    }
+}
+
+fn load_local_winfsp() -> Result<()> {
     unsafe {
-        if LoadLibraryW(w!("winfsp-x64.dll")).is_err() {
+        if LoadLibraryW(&get_local_winfsp()).is_err() {
             Err(ERROR_DELAY_LOAD_FAILED.into())
         } else {
-            Ok(FspInit)
+            Ok(())
         }
+    }
+}
+
+fn load_system_winfsp() -> Result<()> {
+    unsafe {
+        let system = get_system_winfsp().ok_or(ERROR_FILE_NOT_FOUND)?;
+        if LoadLibraryW(&system).is_err() {
+            Err(ERROR_DELAY_LOAD_FAILED.into())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+/// Initialize WinFSP.
+pub fn winfsp_init() -> Result<FspInit> {
+    if  load_system_winfsp().is_err() {
+        Err(ERROR_DELAY_LOAD_FAILED.into())
+    } else {
+        Ok(FspInit)
     }
 }
 
