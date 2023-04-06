7fbe481b63022ddccfe594f82e36b914f013e40d
TimeEngineer <chenpascal241096@yahoo.fr>
2023-04-06 17:56:46 +0100

    winfsp: reexport windows (needed by FileSystemContext)

--- winfsp/src/lib.rs
+++ winfsp/src/lib.rs
@@ -14,8 +14,8 @@ pub use error::FspError;
 pub use error::Result;
 
 pub use init::{winfsp_init, winfsp_init_or_die, FspInit};
-
 pub use widestring::{U16CStr, U16CString};
+pub use windows::*;
 
 #[cfg(feature = "delayload")]
 pub mod build {
